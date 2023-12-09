df4020389055f59d6ab71f59cf4c70a12a71e5f5
mcapitanio <matteo.capitanio@gmail.com>
2023-12-09 14:16:30 +0100

    update

--- detectron2/modeling/meta_arch/build.py
+++ detectron2/modeling/meta_arch/build.py
@@ -18,6 +18,7 @@ def build_model(cfg):
     Build the whole model architecture, defined by ``cfg.MODEL.META_ARCHITECTURE``.
     Note that it does not load any weights from ``cfg``.
     """
+    torch.multiprocessing.set_start_method('spawn', force=True)
     meta_arch = cfg.MODEL.META_ARCHITECTURE
     model = META_ARCH_REGISTRY.get(meta_arch)(cfg)
     model.to(torch.device(cfg.MODEL.DEVICE))
